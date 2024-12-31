6fe1b8aa530ea214d965a1c2f7c50e1ef773d49b
NYM <101108705+nymur-rahman-nyar@users.noreply.github.com>
2024-12-31 15:42:34 -0700

    added comment

--- eagle.py
+++ eagle.py
@@ -4,9 +4,15 @@ import my_functions
 
 my_url = "https://www.vasonite.com/"
 
+# filter the top level domain 
 top_level_domain = my_functions.top_level_domain(my_url)
 
+
+# get the IP Address 
 ip_address = my_functions.get_ip(top_level_domain)
+
+
+
 
 
 print(f"output: {top_level_domain}\nIP: {str(ip_address)}")
